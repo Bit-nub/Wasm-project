218e537db7709f68aa364064bcc943952e715a72
x) <meduse@theos.meduse>
2022-06-16 16:29:10 +0200

    2.0

--- wasmit.sh
+++ wasmit.sh
@@ -98,6 +98,7 @@ mkdir $pathToScript$path1 && mkdir $pathToScript$path2 && mkdir $pathToScript$pa
 
 cd && curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
 source ~/.bashrc
+source $HOME/.cargo/env
 rustup install 1.43.0 > /dev/null 2>&1
 rustup override set 1.43.0 
 
@@ -167,7 +168,7 @@ name="${nameext%%.*}"
 cp $i $home/wasm-binary-security/tool/wasm-security-analysis
 
 cd $home/wasm-binary-security/tool/wasm-security-analysis && cargo clean > /dev/null 2>&1
-cd $home/wasm-binary-security/tool/wasm-security-analysis && cargo run  > /dev/null 2>&1 $nameext >> $name"-analysis.txt" && echo "static analysis dump file has been created for $nameext."
+cd $home/wasm-binary-security/tool/wasm-security-analysis && cargo run  > /dev/null 2>&1 $nameext >> $name"-analysis.txt" && echo "static analysis dump file has been created for $nameext"
 
 #path=$(echo $i | cut -c 2-)
 #path=${pathnameext#"$namext"}
