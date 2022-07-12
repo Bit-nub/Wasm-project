78d4dc9afdc575d1410e891ee8b2b982fe220831
x) <meduse@theos.meduse>
2022-07-12 06:30:26 +0200

    plot.py update; get_general_attributes() to include all chunks

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ from attr import attr
 import pandas as pd
 from IPython.display import display
 import numpy as np
+import re
 
 homeDir = os.getenv('HOME')
 rootDir=str(homeDir)+"/Wasm-project/out/"
@@ -40,17 +41,15 @@ def get_general_attributes():
     table_name=['Cheerp_tool_chain','Emscripten_tool_chain','Clang/llvm_tool_chain','Wasi_tool_chain']
     
     column_entries=get_source_names(rootDir)
-    line_entries=['Functions','Imported','Non-imported','Exported','Tables','Table entries at init','Of those unique functions','Instructions','call','call_indirect']
+    line_entries=['Functions','Imported','Non-imported','Exported','Tables','Table entries at init','Of those unique functions','Instructions','call','call_indirect','Globals','Likely Stack Pointer (global id)','Functions using stack pointer','Functions w/ stack allocation','Unique Function Types','Functions w/ AL. one indirect call','Classes']
     
     df_emcc=pd.DataFrame(index=line_entries, columns=column_entries)
     df_wasi=pd.DataFrame(index=line_entries, columns=column_entries)
     df_llvm=pd.DataFrame(index=line_entries, columns=column_entries)
     df_cheerp=pd.DataFrame(index=line_entries, columns=column_entries)
     
-    #print(source_name)
-    #print(df)
-    chunkname=['chunk2.txt','chunk3.txt','chunk4.txt']
-    for item in chunkname:
+    chunkname=['chunk2.txt','chunk3.txt','chunk4.txt','chunk5.txt','chunk6.txt','chunk8.txt','chunk9.txt','chunk12.txt','chunk13.txt']
+    for item in chunkname[0:3]:
         itm1_file= open(os.path.join(chunksDir,str(item)),"r+")
         file = itm1_file.read()
         for paragraph in file.strip().split("\n\n"):
@@ -100,6 +99,109 @@ def get_general_attributes():
                         src=src_comp.replace("wasi","")
 
                     #print("filename_compilatorname : ",src_comp)
+    for item in chunkname[3:]:
+        itm1_file= open(os.path.join(chunksDir,str(item)),"r+")
+        file = itm1_file.read()
+        for paragraph in file.strip().split("\n\n"):
+            src_comp,src,comp,lsp,fsp,fsa,uft,fic="","","","","","","",""
+            i,j=0,0
+            if "Globals" in paragraph: #chunk5
+                for line in paragraph.split("\n"):
+                    if ".txt" in line:
+                        src_comp=line.split("-")[0]
+                        digit=re.search(r'\d',src_comp)
+                        comp=src_comp.split(digit.group())[1]
+                        src=src_comp.replace(comp,"")
+                    if "#" in line:
+                        i+=1
+                if comp == "emcc":
+                    df_emcc.at['Globals',src]=i
+                if comp == "cheerp":
+                    df_cheerp.at['Globals',src]=i
+                if comp == "llvm":
+                    df_llvm.at['Globals',src]=i
+                if comp == "wasi":
+                    df_wasi.at['Globals',src]=i
+            if "Likely the stack pointer" in paragraph: #chunk6
+                for line in paragraph.split("\n"):
+                    if ".txt" in line:
+                        src_comp=line.split("-")[0]
+                        digit=re.search(r'\d',src_comp)
+                        comp=src_comp.split(digit.group())[1]
+                        src=src_comp.replace(comp,"")
+                    if "Likely the stack pointer" in line:
+                        lsp=line.split(",")[1].strip(" '[]")
+                    if "Functions using stack pointer" in line : 
+                        fsp=line.split(",")[1].split("(")[0].strip(" '")
+                    if "functions with stack allocation total" in line :
+                        fsa=line.split(",")[1].split("(")[0].strip(" '")
+                if comp == "emcc":
+                    df_emcc.at['Likely Stack Pointer (global id)',src]=lsp
+                    df_emcc.at['Functions using stack pointer',src]=fsp
+                    df_emcc.at['Functions w/ stack allocation',src]=fsa
+                if comp == "cheerp":
+                    df_cheerp.at['Likely Stack Pointer (global id)',src]=lsp
+                    df_cheerp.at['Functions using stack pointer',src]=fsp
+                    df_cheerp.at['Functions w/ stack allocation',src]=fsa
+                if comp == "llvm":
+                    df_llvm.at['Likely Stack Pointer (global id)',src]=lsp
+                    df_llvm.at['Functions using stack pointer',src]=fsp
+                    df_llvm.at['Functions w/ stack allocation',src]=fsa
+                if comp == "wasi":
+                    df_wasi.at['Likely Stack Pointer (global id)',src]=lsp
+                    df_wasi.at['Functions using stack pointer',src]=fsp
+                    df_wasi.at['Functions w/ stack allocation',src]=fsa
+            if "Counts of function types" in paragraph: #chunk8
+                for line in paragraph.split("\n"):
+                    if ".txt" in line:
+                        src_comp=line.split("-")[0]
+                        digit=re.search(r'\d',src_comp)
+                        comp=src_comp.split(digit.group())[1]
+                        src=src_comp.replace(comp,"")
+                    if "Counts of function types" in line:
+                        uft=line.split("(")[1].split()[0].strip()
+                if comp == "emcc":
+                    df_emcc.at['Unique Function Types',src]=uft
+                if comp == "cheerp":
+                    df_cheerp.at['Unique Function Types',src]=uft
+                if comp == "llvm":
+                    df_llvm.at['Unique Function Types',src]=uft
+                if comp == "wasi":
+                    df_wasi.at['Unique Function Types',src]=uft
+            if "Functions with at least one call_indirect" in paragraph: #chunk9
+                for line in paragraph.split("\n"):
+                    if ".txt" in line:
+                        src_comp=line.split("-")[0]
+                        digit=re.search(r'\d',src_comp)
+                        comp=src_comp.split(digit.group())[1]
+                        src=src_comp.replace(comp,"")
+                    if "Functions with at least one call_indirect" in line:
+                        fic=line.split(",")[1].split("(")[0].strip(" '")
+                if comp == "emcc":
+                    df_emcc.at['Functions w/ AL. one indirect call',src]=fic
+                if comp == "cheerp":
+                    df_cheerp.at['Functions w/ AL. one indirect call',src]=fic
+                if comp == "llvm":
+                    df_llvm.at['Functions w/ AL. one indirect call',src]=fic
+                if comp == "wasi":
+                    df_wasi.at['Functions w/ AL. one indirect call',src]=fic
+            if "call_indirect target equivalence classes" in paragraph: #chunk5
+                for line in paragraph.split("\n"):
+                    if ".txt" in line:
+                        src_comp=line.split("-")[0]
+                        digit=re.search(r'\d',src_comp)
+                        comp=src_comp.split(digit.group())[1]
+                        src=src_comp.replace(comp,"")
+                    if "#" in line:
+                        j+=1
+                if comp == "emcc":
+                    df_emcc.at['Classes',src]=j
+                if comp == "cheerp":
+                    df_cheerp.at['Classes',src]=j
+                if comp == "llvm":
+                    df_llvm.at['Classes',src]=j
+                if comp == "wasi":
+                    df_wasi.at['Classes',src]=j
     print("\n -- cheerp table :\n")
     display(df_cheerp.to_string())  
     print("\n -- llvm table :\n")
@@ -111,7 +213,7 @@ def get_general_attributes():
     #needs nb of globals/ nb of classes/ which gb is the stack pointer /nb of unique func types / chunk 9 
 
 
-#get_general_attributes()
+get_general_attributes()
 
 def get_source_tool_names(rootDir) :
     #source names define colum entries in each tool chain's table (line entries are the attributes we get)
@@ -209,13 +311,13 @@ def get_count_of_func_types():
                     func_type_pct.append(line.split("×")[0].strip(" '[").split()[1].strip("()"))
                     func_type.append(line.split("×")[1].strip().split("'")[0].strip())
             idx=len(func_type_count)
-            df_cft=pd.DataFrame(index=range(idx),columns=['function_type','type_count','type_count_%_'])
-            df_cft.at[:,'function_type']=func_type
-            df_cft.at[:,'type_count']=func_type_count
-            df_cft.at[:,'type_count_%_']=func_type_pct
+            df_uft=pd.DataFrame(index=range(idx),columns=['function_type','type_count','type_count_%_'])
+            df_uft.at[:,'function_type']=func_type
+            df_uft.at[:,'type_count']=func_type_count
+            df_uft.at[:,'type_count_%_']=func_type_pct
             print("\n",src_comp)
             print("have ",ufc," unique_types")
-            display(df_cft.to_string())
+            display(df_uft.to_string())
 
         #returns df and 2d tuple containing src_comps + ufcs for general attributes            
             
@@ -256,4 +358,4 @@ def get_init_tables():
             display(df_it.to_string())
         # returns df and 2d tuple src_comps + tirs for general attributes
 
-get_init_tables()
+#get_init_tables()
